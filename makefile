88d8202c696aae17fd505e3cb4632e1d38462861
Aiko Bernehed <aiko.bernehed@hhu.de>
2016-01-27 17:50:17 +0100

    solved lab class

---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	g++ -O3 -pedantic -o nlsi nlsi.cxx
--- nlsi.cxx
+++ nlsi.cxx
@@ -12,6 +12,9 @@ void init( cmplx* const psi0, const double eta, const double sigma, const double
 
 void writeToFile(const cmplx* const v, const string s, const double dx,
                  const int Nx, const double xmin);
+
+void lin_step (cmplx* const psi0, const double dt, const double dx, const int Nx);
+void nonlin_step (cmplx* const psi0, const double dt, const int Nx);
 //-----------------------------------
 int main(){
 
@@ -37,8 +40,16 @@ int main(){
 
 	for (int i = 1; i <= Na; i++) {
 
+		lin_step(psi0, dt/2., dx, Nx);
+
 		for (int j = 1; j <= Nk-1; j++) {
+			nonlin_step (psi0, dt, Nx);
+			lin_step (psi0, dt, dx, Nx);
 		}
+
+		nonlin_step (psi0,  dt, Nx);
+		lin_step (psi0, dt/2., dx, Nx);
+
 		strm.str("");
 		strm << "psi_" << i;
 		writeToFile(psi0,strm.str(), dx,Nx,xmin);
@@ -67,5 +78,44 @@ void init( cmplx* const psi0, const double eta,  const double dx, const double d
 	for(int i=0;i<Nx; i++){
 		double x = i*dx - x0;
 		psi0[i] = 2*f/cosh(eta * x);
+	}
+}
+
+void lin_step (cmplx* const psi0, const double dt, const double dx, const int Nx) {
+
+	const cmplx alpha = cmplx(0.0, -dt/(dx*dx));
+
+	cmplx* o = new cmplx[Nx];
+	cmplx* r = new cmplx[Nx];
+
+	for (int i=0; i<Nx; i++) {
+		r[i] = psi0[i];
+	}
+
+	o[0] = -alpha/(1.0+2.0*alpha);
+	r[0] = r[0]/(1.0+2.0*alpha);
+
+	for (int i=1; i<Nx; i++) {
+		o[i] = -alpha/(1.0+2.0*alpha + alpha*o[i-1]);
+		r[i] = (r[i] + alpha*r[i-1])/(1.0+2.0*alpha + alpha*o[i-1]);
+	}
+
+	psi0[Nx-1] = r[Nx-1];
+
+	for (int i=2; i<=Nx; i++) {
+		psi0[Nx-i] = r[Nx-i] - o[Nx-i]*psi0[Nx-i+1];
+	}
+
+	delete[] r;
+	delete[] o;
+
+}
+
+void nonlin_step (cmplx* const psi0, const double dt, const int Nx) {
+
+	const cmplx expt = cmplx (0.0, dt);
+
+	for (int i=0; i<Nx; i++) {
+		psi0[i] = psi0[i] * exp(expt*pow(abs(psi0[i]),2));
 	}
 }
